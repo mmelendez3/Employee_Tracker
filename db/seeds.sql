8d4c4fef68f41955140e9f2426c971a441e377bd
mmelendez3 <mele0019@gmail.com>
2021-11-08 22:57:37 -0600

    added inquirer

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,18 +11,19 @@ VALUES
 ('Lead Engineer', 150000, 2),
 ('Sales Person', 80000, 1), 
 ('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3),
 ('Accountant', 125000, 3),
 ('Legal Team Lead', 250000, 4),
 ('Lawyer', 190000, 4);
 
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('John', 'Doe', 1, 1),
-('Mike', 'Chan', 3, 2),
-('Ashley', 'Rodriguez', 2, 1), 
-('Kevin', 'Tupik', 4, 2),
-('Malia', 'Brown', 5, 3),
-('Sarah','Lourd', 6, 4),
-('Tom', 'Allen', 7, 4),
-('Christian','Erikson', 2, 4);
-
+('John', 'Doe', 1, NULL),
+('Mike', 'Chan', 2, 1),
+('Ashley', 'Rodriguez', 3, NULL),
+('Kevin', 'Tupik', 4, 3),
+('Kunal', 'Singh', 5, NULL),
+('Malia', 'Brown', 6, 5),
+('Sarah', 'Lourd', 7, NULL),
+('Tom', 'Allen', 8, 7);
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
+const inquirer = require('inquirer')
+const fs = require('fs')
 const mysql = require('mysql2');
-const cTable = require('console.table');
+require('console.table');
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+
 // Connect to database
 const db = mysql.createConnection(
     {
@@ -20,67 +17,93 @@ const db = mysql.createConnection(
     console.log('Connected to the employee_tracker database.')
   );
 
-  // Get all departments
-app.get('/api/departments', (req, res) => {
-    const sql = `SELECT * FROM department`;
+//   // Get all departments
+// app.get('/api/departments', (req, res) => {
+//     const sql = `SELECT * FROM department`;
   
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-  });
+//     db.query(sql, (err, rows) => {
+//       if (err) {
+//         res.status(500).json({ error: err.message });
+//         return;
+//       }
+//       res.json({
+//         message: 'success',
+//         data: rows
+//       });
+//     });
+//   });
   
-  // Get all roles
-app.get('/api/roles', (req, res) => {
-    const sql = `SELECT role.*, department.name AS department_name
-     FROM role
-     LEFT JOIN department ON role.department_id = department.id`;
+//   // Get all roles
+// app.get('/api/roles', (req, res) => {
+//     const sql = `SELECT role.*, department.name AS department_name
+//      FROM role
+//      LEFT JOIN department ON role.department_id = department.id`;
   
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-  });
+//     db.query(sql, (err, rows) => {
+//       if (err) {
+//         res.status(500).json({ error: err.message });
+//         return;
+//       }
+//       res.json({
+//         message: 'success',
+//         data: rows
+//       });
+//     });
+//   });
   
-  // Get all employees
-app.get('/api/employees', (req, res) => {
-    const sql = `SELECT * FROM employee`;
+//   // Get all employees
+// app.get('/api/employees', (req, res) => {
+//     const sql = `SELECT * FROM employee`;
   
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-  });
+//     db.query(sql, (err, rows) => {
+//       if (err) {
+//         res.status(500).json({ error: err.message });
+//         return;
+//       }
+//       res.json({
+//         message: 'success',
+//         data: rows
+//       });
+//     });
+//   });
+
+  const promptMenu = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'exit']
+        }])
+        .then(userChoice => {
+            switch (userChoice.menu) {
+                case 'view all departments':
+                    selectDepartments();
+                    break;
+                case 'view all roles':
+                    selectRoles();
+                    break;
+                case 'view all employees':
+                    selectEmployees();
+                    break;
+                case 'add a department':
+                    promptAddDepartment();
+                    break;
+                case 'add a role':
+                    promptAddRole();
+                    break;
+                case 'add an employee':
+                    promptAddEmployee();
+                    break;
+                case 'update an employee role':
+                    promptUpdateRole();
+                    break;
+                default:
+                    process.exit();
+            }
+        });
+};
 
   
 
-  // Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
-
-
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+promptMenu();
